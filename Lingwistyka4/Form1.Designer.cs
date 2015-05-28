aade0935fe57e88249e266fa2c403e475623f582
Pawel Czubak <pawel33317@gmail.com>
2015-05-28 19:50:17 +0200

    Sprawdza email, ip i maskę, html tag, dodawanie całkowitych i zespolonych
    Signed-off-by: Pawel Czubak <pawel33317@gmail.com>

---
+++ Lingwistyka4/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Lingwistyka4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " Załaduj plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(620, 178);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zawartość pliku / wprowadzony text";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 241);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(620, 199);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Poprawne";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(649, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "IP i maska";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(649, 62);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(44, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Mail";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(649, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(164, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Dodawanie liczb całkowitych";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(649, 108);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(155, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Dodawanie liczb złozonych";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(648, 131);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(81, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "HTML table";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(648, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Sprawdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(648, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Aktualizuj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(707, 430);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Paweł Czubak 201520";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(648, 154);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(124, 17);
+            this.radioButton6.TabIndex = 15;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "HTML text formatting";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(649, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Generate IP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(649, 300);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Generate mail";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(648, 329);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(124, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Generate digits";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(648, 358);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(124, 23);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "Generate HTML table";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(648, 387);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(124, 23);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "Generate HTML text";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 452);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lingwistyka zadanie 4 ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Lingwistyka4/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lingwistyka4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string input = "";
+        string[] tablicaZdan;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            int size = 0;
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    input = File.ReadAllText(file);
+                    size = input.Length;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error");
+                }
+                finally
+                {
+                    label1.Text = openFileDialog1.FileName.ToString();
+                }
+            }
+
+            textBox1.Text = "";
+            tablicaZdan = input.Split(new string[] { "\n" }, StringSplitOptions.None);
+            foreach (string s in tablicaZdan)
+            {
+                textBox1.Text += s;
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            tablicaZdan = textBox1.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (string s in tablicaZdan)
+            {
+                textBox3.Text += s;
+                textBox3.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tablicaZdan == null)
+                return;
+            textBox3.Text = "";
+            foreach (string s in tablicaZdan)
+            {
+                Regex regex;
+                Match match;
+                bool poprawne = false;
+
+                if (radioButton1.Checked) //IP
+                {
+                    //^     --> już od początku ciągu musi się zgadzać
+                    //()    --> grupuje
+                    //[0-9] --> zakres znaków
+                    //{1,3} --> musi wystąpić od 1 razu do 3
+                    //[\.]  --> kropka   jeżeli jest w klamrach [] nie trzema poprzedzać \
+                    //{3}   --> całą grupa czyli () musi nastąpić 3 razy
+                    //[ ]   --> spacja
+                    //$     --> po tym ciągu już nic nie może być
+                    regex = new Regex(@"^([0-9]{1,3}[\.]){3}([0-9]){1,3}([ ])([0-9]{1,3}[\.]){3}([0-9]){1,3}$");
+                    match = regex.Match(s);
+
+                    if (match.Success)
+                    {
+                        string[] SplittedBySpace = match.ToString().Split(new string[] { " " }, StringSplitOptions.None);
+                        string[] IPSplittedByDOT = SplittedBySpace[0].Split(new string[] { "." }, StringSplitOptions.None);
+                        string[] MaskSplittedByDOT = SplittedBySpace[1].Split(new string[] { "." }, StringSplitOptions.None);
+                        //sprawdza IP czy ok JAK OK ustawia na true
+                        if (int.Parse(IPSplittedByDOT[0]) < 256 && int.Parse(IPSplittedByDOT[1]) < 256 && int.Parse(IPSplittedByDOT[2]) < 256 && int.Parse(IPSplittedByDOT[3]) < 256)
+                        {
+                            poprawne = true;
+                        }
+
+                        //sprawdza maskę jak znajdzie błąd przestawia znów na false
+                        //czy poprzednie człony maski są równe 255 jeżeli kolejny za nimi nie jest 0 
+                        if (int.Parse(MaskSplittedByDOT[3]) > 0 )
+                            if (int.Parse(MaskSplittedByDOT[2]) != 255)
+                                poprawne = false;
+                        if (int.Parse(MaskSplittedByDOT[2]) > 0)
+                            if (int.Parse(MaskSplittedByDOT[1]) != 255)
+                                poprawne = false;
+                        if (int.Parse(MaskSplittedByDOT[1]) > 0)
+                            if (int.Parse(MaskSplittedByDOT[0]) != 255)
+                                poprawne = false;
+
+
+                        //czy mają możliwe wartości maski: 0, 128, 192, 224, 240, 248, 252, 254, 255
+                        foreach (string m in MaskSplittedByDOT)
+                        {
+                            int n = int.Parse(m);
+                            if (n != 0 && n != 128 && n != 192 && n != 224 && n != 240 && n != 248 && n != 252 && n != 254 && n != 255)
+                                poprawne = false;
+                        }
+
+
+                        
+                    }
+                }
+                if (radioButton2.Checked) //MAIL
+                {
+                    //[a-zA-Z0-9_.+-] może wystąpić1 lub więcej razy przez "+"
+                    //@  musi wystąpić znak @
+                    //\. musi być kropka
+                    //[a-zA-Z0-9-.]+ wielokrotnie końcówka domeny
+                    regex = new Regex(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                }
+                if (radioButton3.Checked) //ADD INTEGER
+                {
+                    //0-9 z + dowolną ilość razy
+                    //na końcu cyfra
+                    regex = new Regex(@"^([0-9]+\+)+[0-9]+$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                }
+                if (radioButton4.Checked) //ADD COMPLEX
+                {
+                    //? raz lub w ogóle
+                    //| lub lewa lub prawa
+                    regex = new Regex(@"^(([0-9]+[i]?|[i])\+)+([0-9]+[i]?|i)$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                }
+                if (radioButton5.Checked) //HTML TABLE
+                {
+                    //[^<]*  wszystko tylko nie < dowolną iloość razy lub w ogóle
+                    //(.*) wszystko ile razy się chce
+                    //<table --> ten tekst 
+                    //([ ]([^<])*>)|(>)   spacja i jakiś tekst lub tylko spacja i znak ">" | znak ">"
+                    regex = new Regex(@"^<table(([ ]([^<])*>)|(>))(.*)</table>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                }
+                if (radioButton6.Checked) //HTML A, P B SRTONG H1 EM I 
+                {
+                    regex = new Regex(@"^<a(([ ]([^<])*>)|(>))(.*)</a>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                    regex = new Regex(@"^<p(([ ]([^<])*>)|(>))(.*)</p>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                    regex = new Regex(@"^<b(([ ]([^<])*>)|(>))(.*)</b>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                    regex = new Regex(@"^<strong(([ ]([^<])*>)|(>))(.*)</strong>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                    regex = new Regex(@"^<em(([ ]([^<])*>)|(>))(.*)</em>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                    regex = new Regex(@"^<i(([ ]([^<])*>)|(>))(.*)</i>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                    regex = new Regex(@"^<h1(([ ]([^<])*>)|(>))(.*)</h1>$");
+                    match = regex.Match(s);
+                    if (match.Success)
+                        poprawne = true;
+                }
+
+
+                //jak sprawdzenie ok wypisz jak nie też
+                if (poprawne)
+                {
+                    textBox3.Text += "POPRAWNY----->: " + s;
+                }
+                else
+                {
+                    textBox3.Text += "NIEPOPRAWNY-->: " + s;
+                }
+                textBox3.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            tablicaZdan = new string[] { 
+                "10.0.0.1 255.255.255.0",
+                "22.22.22.22 22.22.22.22",
+                "123.132.289.22 255.255.255.255",
+                "1.1.1..1 3.3.3.3",
+                "1.1.1.1 3.3.s.3",
+                "266.0.0.1 255.255.255.0",
+                "26.0.0.1 255.255.255.133",
+                "26.0.0.1 255.255.255.128",
+                "266.0.0.1 255.255.128.128"
+            };
+            foreach (string s in tablicaZdan)
+            {
+                textBox1.Text += s;
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            tablicaZdan = new string[] { 
+                "pawel33317@gmail.com",
+                "pawel@pawel@.wp.pl",
+                "pawel@pl",
+                "@wp.pl",
+                "pawe#$%@wp.pl",
+                "pawel@o2.com$"
+            };
+            foreach (string s in tablicaZdan)
+            {
+                textBox1.Text += s;
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            tablicaZdan = new string[] { 
+                "223+1232+324",
+                "223+1+1+",
+                "1+0",
+                "+1+0",
+                "2i+7+6+3+i+22",
+                "i+i",
+                "4i+3i+1",
+                "i+ii",
+                "i+3+i+i+i",
+                "33+i+33+2",
+                "3*i+5"
+            };
+            foreach (string s in tablicaZdan)
+            {
+                textBox1.Text += s;
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            tablicaZdan = new string[] { 
+                "<table>dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<table>dfdsaf<>\"\"#$AS.m,bJNV     xcv  <table>",
+                "<\table>dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<table>dfdsaf<>\"\"#$AS.m,bJNV     xcv  </tablfe>",
+                "<table id=\"adsf asd\">dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<table weight='100px;'>dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<table asd das >dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<tableasdf sdf >dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<table df  fd fdsa fsd gfsfd=''>dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>",
+                "<tableasdf <a></a> >dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>"
+            };
+            foreach (string s in tablicaZdan)
+            {
+                textBox1.Text += s;
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            tablicaZdan = new string[] { 
+                "<a href='#'>Link</a>",
+                "<p class='klasa'>Link</p>",
+                "<b>Link</b>",
+                "<b>Link</a>",
+                "<ccc>Link</ccc>",
+                "<h1>Link</h1>",
+                "<a>Link<a>",
+                "<strong>Link</strong>",
+                "<table>dfdsaf<>\"\"#$AS.m,bJNV     xcv  </table>"
+            };
+            foreach (string s in tablicaZdan)
+            {
+                textBox1.Text += s;
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
